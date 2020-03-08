d42e2b4744cbf7d18cc263f0c39b9a3313ba094e
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-03-08 11:42:46 -0500

    Enter page complete

--- projectFrame/app.js
+++ projectFrame/app.js
@@ -1,6 +1,23 @@
-const $enterScreen = $('<section>').css('width', '100vw').css('height', '100vh').css('background', 'yellow').css('border', 'solid').css('display', 'flex')
-let $square = $('<div>').css('width', '30vw').css('height', '30vh').css('border', 'black')
-$enterScreen.append('$square')
-let $button = $('<button>')
-$('body').prepend($enterScreen)
+// entry screen
+const $enterScreen = $('<section>').css({'width': '100vw', 'height': '100vh', 'background-size': 'cover', 'display': 'flex', 'align-items': 'center', 'justify-content': 'center', 'box-shadow': '5px 5px 10px #888888',}).addClass('background')
+            
+let $square = $('<div>').css({'width':'100vw','height':'100vh','display':'flex','align-items':'center','justify-content': 'center', 'background':'rgba(230,230,230,0.3)',})
+let $button = $('<button>').html('ENTER').css('font-size', '20em').css('letter-spacing', '2%').css('font-family', 'Barrio').css('background-color', 'white').css('border', 'solid')
+$('body').prepend($enterScreen) 
+$enterScreen.append($square)
+$square.append($button)
+$('main').hide()
+// button functions
+$button.mouseover(function(){
+    $button.css('background', 'rgb(83, 177, 231)').css('box-shadow', '10px 10px 15px #111111')
+})
+$button.mouseout(() => $button.css('background-color', 'white').css('border', 'solid').css('box-shadow', '5px 5px 10px #888888'))
+$button.on('click')
 
+$button.click(function(){
+    $enterScreen.slideUp()
+    $('main').show('slow')
+}); 
+
+
+ 
