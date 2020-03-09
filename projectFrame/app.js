2dc429bdda2c2c8aea87c291c70982bf436a2161
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-03-09 13:33:18 -0500

    linked api for portfolio

--- projectFrame/app.js
+++ projectFrame/app.js
@@ -6,6 +6,46 @@ $('body').prepend($enterScreen)
 $enterScreen.append($square)
 $square.append($button)
 $('main').hide()
+let $portfolioButton = $('.portfolioButton')
+let $portfolioContainer = $('article:nth-of-type(2)')
+let $portfolioDiv=$('<div>')
+let $returnButton = $('<button>').html('Click me to go back!')
+
+//create portfolio functions
+function addPortfolioApp(projects){
+    console.log('app-projects', projects)
+        $portfolioDiv.css({'width':'80%', 'height': '80%','border': 'solid','display': 'flex','align-items': 'center','justify-content': 'space-between', 'flex-direction':'row', 'flex-wrap':'wrap','box-shadow': '5px 5px 10px #888888'})
+        for (let i = 0; i < projects.length; i++) {
+            let $card = `<div class="card">
+                <img class="pImg" src="${projects[i].image}"></img>
+                <div class="cardOverlay">
+                    <h4>${projects[i].title}</h4>
+                    <p>${projects[i].description}</p>
+                    <a href="${projects[i].url}" target="_blank">Check me out!</a>
+                </div>
+            </div>`
+            $portfolioDiv.append($card)
+        }
+        $portfolioContainer.append($portfolioDiv)
+  }
+
+function fetchPortfolio(){
+    let source = `https://spreadsheets.google.com/feeds/list/1WyGCKMlb_Wjs-_k6ZnE4cY0yhIPW3CvR4YogUg9QAQs/od6/public/values?alt=json`;
+    fetch(source)
+      .then(res => res.json())
+      .then(data => {
+            let projects = data.feed.entry.map(project => {
+              return{
+                title: project.gsx$title.$t,
+                image: project.gsx$image.$t,
+                description: project.gsx$description.$t,
+                url: project.gsx$url.$t,
+              }
+            })
+            addPortfolioApp(projects)
+      })}
+
+    
 // button functions
 $button.mouseover(function(){
     $button.css('background', 'rgb(83, 177, 231)').css('box-shadow', '10px 10px 15px #111111')
@@ -18,19 +58,13 @@ $button.click(function(){
     $('main').show('slow')
 }); 
 
-$portfolioButton = $('.portfolioButton')
-console.log("$portfolioButton", $portfolioButton)
-$portfolioContainer = $('article:nth-of-type(2)')
-console.log("$portfolioContainer", $portfolioContainer)
-$portfolioDiv=('<div>')
-$buttonDiv=('<div>')
-$returnButton = $('<button>').html('Click me to go back!')
-
 $portfolioButton.click(function(){
     $(this).hide()
     $returnButton.show()
-    $portfolioContainer.css('height', '100vh')
+    $portfolioContainer.css({'max-height': '100vw','display':'flex', 'flex-direction': 'column', 'align-items': 'center'})
+    fetchPortfolio();
     $portfolioContainer.append($returnButton)
+    
 })
 
 $returnButton.click(function(){
@@ -39,3 +73,45 @@ $returnButton.click(function(){
     $portfolioContainer.css('height', 'auto')
 })
 
+{/* <div class="card">
+    <img src="${projects[i].img}"></img>
+    <div class="cardOverlay">
+        <h4>${projects[i].title}</h4>
+        <a href="${projects[i].url}" target="_blank">Check me out!</a>
+    </div>
+</div> */}
+
+
+    // for (let i = 0; i < projects.length; i++) {
+    //     let $card = `<div class="card">
+    //         <img class="pImg" src="${projects[i].img}"></img>
+    //         <div class="cardOverlay">
+    //             <h4>${projects[i].title}</h4>
+    //             <a href="${projects[i].url}" target="_blank">Check me out!</a>
+    //         </div>
+    //     </div>`
+    //     $portfolioDiv.append($card)
+    // }
+
+
+// onst createTable = () => {
+//     for (let i = 0; i < iconArray.length; i++) {
+//         let $cardArray = $(`
+//                        <a href="#">
+//                            <div class="ImageContainer"><i class="${iconArray[i].icon}"></i></div>
+//                            <div class="ItemContentContainer">
+//                              <h4 class="ItemHeader">${iconArray[i].title}</h4>
+//                              <p class="ItemHeading">${iconArray[i].description}</p>
+//                            </div>
+//                        </a>
+//                      </li>`)
+//         $ul.append($li);
+//     }
+// };
+{/* function addPortfolioApp(projects){
+    console.log('app-projects', projects)
+        let $card = $('<div>').css({'width': '20%', 'height': '30%', 'border':'solid'})
+        $portfolioDiv.css({'width':'80%', 'height': '80%','border': 'solid','display': 'flex','align-items': 'center','justify-content': 'space-between', 'box-shadow': '5px 5px 10px #888888'})
+        $portfolioDiv.append($card)
+        $portfolioContainer.append($portfolioDiv)
+  } */}
